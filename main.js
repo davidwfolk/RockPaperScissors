b25e66626fcb9f8dc9a62427875d0576b47bcb64
David Folk <davidwfolk@gmail.com>
2020-03-18 17:18:33 -0600

    working on showing results

--- main.js
+++ main.js
@@ -16,24 +16,6 @@ let player1 = {
   ],
 }
 
-// let computer = {
-//   name: "computer",
-//   roshambo: [
-//     {
-//       img: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Rock-paper-scissors_%28rock%29.png",
-//         status: "rock",
-//       },
-//       {
-//       img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Rock-paper-scissors_%28paper%29.png/1200px-Rock-paper-scissors_%28paper%29.png",
-//         status: "paper",
-//       },
-//       {
-//       img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Rock-paper-scissors_%28scissors%29.png/600px-Rock-paper-scissors_%28scissors%29.png",
-//         status: "scissors",
-//     },
-//   ],
-// }
-
 let choices = ["rock", "paper", "scissors"]
 
   function playerPhotos(playerChoice) {
@@ -103,25 +85,6 @@ function draw(winnertext) {
   resultElem.innerText = winnertext
 }
 
-
-
-
-// function dontworry() {
-//   let playerRockElem = document.getElementById("btn-rock")
-//   let playerPaperElem = document.getElementById("btn-paper")
-//   let playerScissorsElem = document.getElementById("btn-scissors")
-//   let rpsElem = document.getElementById("rps-image")
-
-
-//   if (playerRockElem) {
-//     return player1.roshambo[0].img
-//   }
-//   else if (playerPaperElem) {
-//     return player1.roshambo[1].img
-//   }
-//   else if (playerScissorsElem) {
-//     return player1.roshambo[2].img
-//   }
-//   console.log(play)
-// }
-
+function resultCount() {
+  
+}
