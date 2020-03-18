599fbb489ba82eaf555bdaae11d3c3a424a61760
David Folk <davidwfolk@gmail.com>
2020-03-18 12:00:29 -0600

    buttons & players sorted

---
+++ main.js
@@ -0,0 +1,35 @@
+let player1 = {
+  name: "JimBob",
+  roshambo: [
+    {
+      img: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Rock-paper-scissors_%28rock%29.png",
+        status: "rock",
+      },
+      {
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Rock-paper-scissors_%28paper%29.png/1200px-Rock-paper-scissors_%28paper%29.png",
+        status: "paper",
+      },
+      {
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Rock-paper-scissors_%28scissors%29.png/600px-Rock-paper-scissors_%28scissors%29.png",
+        status: "scissors",
+    },
+  ],
+}
+
+let player2 = {
+  name: "JimBob",
+  roshambo: [
+    {
+      img: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Rock-paper-scissors_%28rock%29.png",
+        status: "rock",
+      },
+      {
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Rock-paper-scissors_%28paper%29.png/1200px-Rock-paper-scissors_%28paper%29.png",
+        status: "paper",
+      },
+      {
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Rock-paper-scissors_%28scissors%29.png/600px-Rock-paper-scissors_%28scissors%29.png",
+        status: "scissors",
+    },
+  ],
+}
